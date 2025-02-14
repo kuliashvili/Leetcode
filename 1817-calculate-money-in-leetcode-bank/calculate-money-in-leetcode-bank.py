1e67eeed1027a087db98a21aac5b5a86b69e6864
Giorgi Kuliashvili <93291077+kuliashvili@users.noreply.github.com>
2025-02-14 12:13:52 +0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (37.69%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.py
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.py
@@ -1,14 +1,13 @@
 class Solution:
     def totalMoney(self, n: int) -> int:
-        countMoney = 0
+        totalMoney = 0
         start = 1
         weekday = 0
-
+        
         for i in range(n):
-            countMoney += start + weekday
-            weekday += 1
+            totalMoney += start + weekday
+            weekday +=1
             if weekday == 7:
                 start += 1
                 weekday = 0
-
-        return countMoney        
+        return totalMoney        
