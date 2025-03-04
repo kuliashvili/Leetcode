20846ebc3579f82cdce66efaeea4c045f9505555
Giorgi Kuliashvili <93291077+kuliashvili@users.noreply.github.com>
2025-03-04 14:28:29 +0400

    Time: 0 ms (100.00%) | Memory: 53.7 MB (8.43%) - LeetSync

--- 1490-generate-a-string-with-characters-that-have-odd-counts/generate-a-string-with-characters-that-have-odd-counts.js
+++ 1490-generate-a-string-with-characters-that-have-odd-counts/generate-a-string-with-characters-that-have-odd-counts.js
@@ -3,17 +3,5 @@
  * @return {string}
  */
 var generateTheString = function(n) {
-    let string = ""
-    if(n % 2 !== 0){
-        for(let i = 0; i < n; i++){
-            string += "a"
-        }
-        return string
-    }else{
-        for(let i = 0; i < n -1 ; i++){
-            string += "a"
-        }
-        string += "b"
-        return string
-    } 
+    return "a".repeat(n - 1) + (n % 2 === 0 ? "b" : "a")
 };
