328bf5050e4c4de243285e9326f1a0a58049add4
Giorgi Kuliashvili <93291077+kuliashvili@users.noreply.github.com>
2025-03-04 15:10:23 +0400

    Time: 1 ms (88.08%) | Memory: 57.9 MB (20.73%) - LeetSync

--- 2819-remove-trailing-zeros-from-a-string/remove-trailing-zeros-from-a-string.js
+++ 2819-remove-trailing-zeros-from-a-string/remove-trailing-zeros-from-a-string.js
@@ -6,12 +6,10 @@ var removeTrailingZeros = function(num) {
     let array = num.split("")
     
     for(let i = array.length -1; i >= 0; i--){
-        if(array[i] !== "0"){
-            return num
-        }
-        if(array[i - 1] != "0"){
-            let answer = num.slice(0, i )
+        if(array[i] != "0"){
+            let answer = num.slice(0, i +1 )
             return answer
         }
     }
+    return num
 };
